b5c503a94729e513bdbe15e9e5ae5fff85ff3e14
liyuefeilong <xhpeng1991@gmail.com>
2015-09-11 01:55:58 +0800

    Rotate Image

--- Rotate Image/Rotate Image/RotateImage.h
+++ Rotate Image/Rotate Image/RotateImage.h
@@ -8,13 +8,11 @@ public:
 	void turnRightImage(vector<vector<int> >& image)
 	{
 		int imaSize = image.size();
-		for (int i = 0; i < imaSize; i++)
+		// 主对角线以下的元素与主对角线上元素沿着主对角线垂直方向对换
+		for (int i = 1; i < imaSize; i++)
 		{
-			for (int j = i; j >= 0; j--)
-			{
-				if (i != j)
-					swap(image[i][j], image[j][i]);
-			}
+			for (int j = i - 1; j >= 0; j--)
+				swap(image[i][j], image[j][i]);
 		}
 		for (int k = 0; k < imaSize / 2; k++)
 		{
