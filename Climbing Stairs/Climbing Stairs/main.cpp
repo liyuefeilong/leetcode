7a42d7ac6cc481347d336f5a85657578174ce506
liyuefeilong <xhpeng1991@gmail.com>
2015-09-15 13:40:29 +0800

    Climbing Stairs

--- Climbing Stairs/Climbing Stairs/main.cpp
+++ Climbing Stairs/Climbing Stairs/main.cpp
@@ -1,9 +1,16 @@
 #include "ClimbingStairs.h"
 #include <iostream>
+using namespace std;
 
 int main()
 {
+	int n;
+	cout << "How many stairs? " << "Input: ";
+	cin >> n;
+	Solution s;
+	int result = s.climbStairs1(n);
 
+	cout << "How many ways to reach the finish line? " "Result£º" << result << endl;
 	system("pause");
 	return 0;
 }
