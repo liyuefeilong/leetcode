eaca90117a8c60c97450d6d91211dd52bbe498e6
liyuefeilong <xhpeng1991@gmail.com>
2015-09-17 13:40:58 +0800

    Linked List Cycle

--- Linked List Cycle/Linked List Cycle/LinkedListCycle.h
+++ Linked List Cycle/Linked List Cycle/LinkedListCycle.h
@@ -12,7 +12,7 @@ class Solution
 public:
 	bool hasCycle(ListNode *head)
 	{
-		if (head == nullptr || head->next == nullptr || head->next->next == nullptr)
+		if (head == nullptr || head->next == nullptr)
 			return false;
 		ListNode* fast = head;
 		ListNode* slow = head;
