0f0c99adc7afeb2264181609b38618c9779649b0
liyuefeilong <xhpeng1991@gmail.com>
2015-09-22 01:19:51 +0800

    Integer to Roman

---
+++ Integer to Roman/Integer to Roman/IntegertoRoman.h
@@ -0,0 +1,11 @@
+#include <string>
+using namespace std;
+
+class Solution
+{
+public:
+	string intToRoman(int num)
+	{
+		const int value[] = {1000, 900, 500};
+	}
+};
---
+++ Integer to Roman/Integer to Roman/main.cpp
@@ -0,0 +1,7 @@
+#include "IntegertoRoman.h"
+
+int main()
+{
+	system("pause");
+	return 0;
+}
