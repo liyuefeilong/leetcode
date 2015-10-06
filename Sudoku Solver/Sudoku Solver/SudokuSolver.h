32250c3639dad561efeb4b66f4d99962cd5c5b33
liyuefeilong <xhpeng1991@gmail.com>
2015-10-06 20:20:42 +0800

    Sudoku Solver

--- Sudoku Solver/Sudoku Solver/SudokuSolver.h
+++ Sudoku Solver/Sudoku Solver/SudokuSolver.h
@@ -2,50 +2,58 @@
 #include <vector>
 using namespace std;
 
-class Solution
-{
+class Solution {
 public:
-	bool isValidSudoku(vector<vector<char> >& board)
-	{
-		for (int i = 0; i < 9; ++i)
+	bool isValidSudoku(vector<vector<char> > &board, int x, int y) {
+
+		// 同一列中是否出现相同的数
+		for (int row = 0; row < 9; ++row) 
+			if ((x != row) && (board[row][y] == board[x][y])) 
+				return false;
+
+		// 同一行中是否出现相同的数
+		for (int col = 0; col < 9; ++col)
+			if ((y != col) && (board[x][col] == board[x][y]))
+				return false;
+
+		// 3 * 3方格中是否出现相同的数
+		for (int row = (x / 3) * 3; row < (x / 3 + 1) * 3; ++row) 
+			for (int col = (y / 3) * 3; col < (y / 3 + 1) * 3; ++col) 
+				if ((x != row) && (y != col) && (board[row][col] == board[x][y])) 
+					return false;
+
+		// 满足条件，则返回true
+		return true;
+	}
+
+	bool internalSolveSudoku(vector<vector<char> > &board) {
+		for (int row = 0; row < 9; ++row) 
 		{
-			for (int j = 0; j < 9; ++j)
+			for (int col = 0; col < 9; ++col) 
 			{
-				if (board[i][j] == '.')
+				if (board[row][col] == '.') 
 				{
-					for (int k = 0; k < 9; ++k)
+					for (int i = 1; i <= 9; ++i)
 					{
-						board[i][j] = '1' + k;
-						if ((check(board, i, j)) && (isValidSudoku(board)))
-							return true;
-						board[i][j] = '.';
+						board[row][col] = '0' + i;
+
+						if (isValidSudoku(board, row, col)) 
+							if (internalSolveSudoku(board)) 
+								return true;
+						// 若当前格子的数不正确，将其重置为'.'
+						// 然后进行下一个尝试
+						board[row][col] = '.';
 					}
+					// 0~9均重复，输出false
 					return false;
 				}
 			}
 		}
-	}
-private:
-	bool check(vector<vector<char> >& board, int x, int y)
-	{
-		for (int i = 0; i < 9; i++)
-		{
-			if ((board[x][i] == board[x][y]) && (i != y))
-				return false;
-		}
-		for (int j = 0; j < 9; j++)
-		{
-			if ((board[j][y] == board[x][y]) && (j != x))
-				return false;
-		}
-		for (int k = x / 3; k < x / 3 + 3; k++)
-		{
-			for (int l = y / 3; l < y / 3 + 3; l++)
-			{
-				if ((board[k][l] == board[x][y]) && ((k != x) || (l != y)))
-					return false;
-			}
-		}
 		return true;
+	}
+
+	void solveSudoku(vector<vector<char> > &board) 
+	{
+		internalSolveSudoku(board);
 	}
 };
--- Sudoku Solver/Sudoku Solver/main.cpp
+++ Sudoku Solver/Sudoku Solver/main.cpp
@@ -51,7 +51,7 @@ int main()
 	vec.clear();
 	vec.push_back('4');
 	vec.push_back('2');
-	vec.push_back('6');
+	vec.push_back('.');
 	vec.push_back('8');
 	vec.push_back('5');
 	vec.push_back('3');
@@ -63,7 +63,7 @@ int main()
 	vec.push_back('7');
 	vec.push_back('1');
 	vec.push_back('3');
-	vec.push_back('9');
+	vec.push_back('.');
 	vec.push_back('2');
 	vec.push_back('4');
 	vec.push_back('8');
@@ -75,7 +75,7 @@ int main()
 	vec.push_back('6');
 	vec.push_back('1');
 	vec.push_back('5');
-	vec.push_back('3');
+	vec.push_back('.');
 	vec.push_back('7');
 	vec.push_back('2');
 	vec.push_back('8');
@@ -88,7 +88,7 @@ int main()
 	vec.push_back('4');
 	vec.push_back('1');
 	vec.push_back('9');
-	vec.push_back('6');
+	vec.push_back('.');
 	vec.push_back('3');
 	vec.push_back('5');
 	board.push_back(vec);
@@ -116,10 +116,17 @@ int main()
 		cout << endl;
 	}
 
-	if (s.isValidSudoku(board))
-		cout << "\nThe Sudoku is valid! " << endl;
-	else
-		cout << "\nThe Sudoku is not valid! " << endl;
+	s.solveSudoku(board);
+
+	cout << "The result is: " << endl;
+	for (int i = 0; i < 9; i++)
+	{
+		for (int j = 0; j < 9; j++)
+		{
+			cout << board[i][j] << " ";
+		}
+		cout << endl;
+	}
 
 	system("pause");
 
