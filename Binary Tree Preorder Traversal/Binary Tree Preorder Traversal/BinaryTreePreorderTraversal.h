da114a4385d6186d337b8f5905f349033b4d38c8
liyuefeilong <xhpeng1991@gmail.com>
2015-10-14 01:53:44 +0800

    Binary Tree Preorder Traversal

--- Binary Tree Preorder Traversal/Binary Tree Preorder Traversal/BinaryTreePreorderTraversal.h
+++ Binary Tree Preorder Traversal/Binary Tree Preorder Traversal/BinaryTreePreorderTraversal.h
@@ -1,10 +1,33 @@
 #include <iostream>
-#include <stack>
+#include <vector>
 
 using namespace std;
+
+struct TreeNode
+{
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL){}
+};
 
 class Solution
 {
 public:
-	
+	void preorder(TreeNode *root, vector<int> &k)
+	{
+		if (root != NULL)
+		{
+		k.push_back(root->val);
+		preorder(root->left, k);
+		preorder(root->right, k);
+		}
+	}
+
+	vector<int> preorderTraversal(TreeNode *root)
+	{
+		vector<int> temp;
+		preorder(root, temp);
+		return temp;
+	}
 };
--- Binary Tree Preorder Traversal/Binary Tree Preorder Traversal/main.cpp
+++ Binary Tree Preorder Traversal/Binary Tree Preorder Traversal/main.cpp
@@ -2,5 +2,17 @@
 
 int main()
 {
-	;
+	TreeNode *p = new TreeNode(5);
+	p->left = new TreeNode(3);
+	p->right = new TreeNode(4);
+	p->left->left = new TreeNode(1);
+
+	Solution s;
+	vector<int> result = s.preorderTraversal(p);
+
+	for (size_t i = 0; i < result.size(); ++i)
+		cout << result[i] << " ";
+	cout << endl;
+	system("pause");
+	return 0;
 }
