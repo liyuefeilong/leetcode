5b215953d26391fe87c71868f903285958d90d86
liyuefeilong <xhpeng1991@gmail.com>
2015-11-27 16:29:08 +0800

    Combination Sum

--- Combination Sum/Combination Sum/CombinationSum.h
+++ Combination Sum/Combination Sum/CombinationSum.h
@@ -9,18 +9,18 @@ class Solution
 public:
 	vector<vector<int> > combinationSum(vector<int> &candidates, int target)
 	{
-		vector<int> temp;
+		vector<int> temp; // 用于存放临时组合
 		sort(candidates.begin(), candidates.end());
 		// 结果要求输出的组合不能重复，需要去除candidates中的重复元素
 		candidates.erase(unique(candidates.begin(), candidates.end()), candidates.end());
-		combination(candidates, temp, 0, target);
+		combinationDFS(candidates, temp, 0, target);
 		return result;
 	}
 
 private:
 	vector<vector<int> > result;
 
-	void combination(vector<int> &candidates, vector<int> &temp, size_t index, int target)
+	void combinationDFS(vector<int> &candidates, vector<int> &temp, size_t index, int target)
 	{
 		if (target == 0)
 		{
@@ -36,7 +36,7 @@ private:
 					return;
 
 				temp.push_back(candidates[i]);
-				combination(candidates, temp, i, target - candidates[i]);
+				combinationDFS(candidates, temp, i, target - candidates[i]);
 				temp.pop_back();
 			}
 		}
