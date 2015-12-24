8983234a6c180e06a3d9ec378ee08d62e8b91ac5
liyuefeilong <xhpeng1991@gmail.com>
2015-12-25 00:40:31 +0800

    Sort Colors

--- Sort Colors/Sort Colors/main.cpp
+++ Sort Colors/Sort Colors/main.cpp
@@ -2,5 +2,29 @@
 
 int main()
 {
-	;
+	int n;
+	cout << "Input the size of objects: ";
+	cin >> n;
+	vector<int> nums(n);
+	cout << "Input the objects: ";
+	for (int i = 0; i < n; ++i)
+	{
+		int a;
+		cin >> a;
+		nums[i] = a;
+	}
+
+	Solution s;
+
+	s.sortColors(nums);
+
+	int SIZE = nums.size();
+
+	cout << "The sorted colors are: ";
+	for (int i = 0; i < SIZE; ++i)
+		cout << nums[i] << " ";
+	cout << endl;
+
+	system("pause");
+	return 0;
 }
